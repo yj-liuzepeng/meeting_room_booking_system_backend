5ba7fe0eb9c3e7fff5695e228db203a69ed5e39b
lzp-mayun <liuzepeng0200@163.com>
2023-11-29 23:34:01 +0800

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ 
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3307,
+      username: "root",
+      password: "Admin22!",
+      database: "meeting_room_booking_system",
+      synchronize: true,
+      logging: true,
+      entities: [],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+          authPlugin: 'sha256_password',
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
